0d151d819d817a67ad88166d541255229212a81e
Coen Adler <co41co@gmail.com>
2024-05-27 18:35:52 -0700

    label based eval

--- utils/eval.py
+++ utils/eval.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torchvision
 from torch.autograd import Variable
 import torch.nn.functional as F
+from sklearn.metrics import average_precision_score, f1_score
 
 from utils.logging import AverageMeter, ProgressMeter
 from utils.adv import pgd_whitebox, fgsm
@@ -49,39 +50,43 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
+    
+def compute_mAP(labels,outputs):
+    AP = []
+    for i in range(labels.shape[0]):
+        AP.append(average_precision_score(labels[i],outputs[i]))
+    return np.mean(AP)
+
+def compute_f1(labels, outputs):
+    outputs = outputs > 0.5
+    return f1_score(labels, outputs, average="samples")
 
 
 def base(model, device, val_loader, criterion, args, writer, epoch=0):
-    """
-        Evaluating on unmodified validation set inputs.
-    """
+
     batch_time = AverageMeter("Time", ":6.3f")
     losses = AverageMeter("Loss", ":.4f")
-    top1 = AverageMeter("Acc_1", ":6.2f")
-    top5 = AverageMeter("Acc_5", ":6.2f")
+    mAP = AverageMeter('mAP', ':6.2f')
+    f1 = AverageMeter('mAP', ':6.2f')
     progress = ProgressMeter(
-        len(val_loader), [batch_time, losses, top1, top5], prefix="Test: "
+        len(val_loader), [batch_time, losses, mAP, f1], prefix="Test: "
     )
 
-    # switch to evaluate mode
+    start_time = time.time()
     model.eval()
 
     with torch.no_grad():
         end = time.time()
-        for i, data in enumerate(val_loader):
-            images, target = data[0].to(device), data[1].to(device)
+        for i, (inputs, targets) in enumerate(val_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
 
-            # compute output
-            output = model(images)
-            loss = criterion(output, target)
-
-            # measure accuracy and record loss
-            acc1, acc5 = accuracy(output, target, topk=(1, 5))
-            losses.update(loss.item(), images.size(0))
-            top1.update(acc1[0], images.size(0))
-            top5.update(acc5[0], images.size(0))
-
-            # measure elapsed time
+            losses.update(loss.item(), inputs.size(0))
+            labels_cpu = targets.cpu().detach().numpy()
+            outputs_cpu = outputs.cpu().detach().numpy()
+            mAP.update(compute_mAP(labels_cpu, outputs_cpu), inputs.size(0))
+            f1.update(compute_f1(labels_cpu, outputs_cpu), inputs.size(0))
             batch_time.update(time.time() - end)
             end = time.time()
 
@@ -92,16 +97,66 @@ def base(model, device, val_loader, criterion, args, writer, epoch=0):
                 progress.write_to_tensorboard(
                     writer, "test", epoch * len(val_loader) + i
                 )
-
-            # write a sample of test images to tensorboard (helpful for debugging)
-            if i == 0 and writer:
-                writer.add_image(
-                    "test-images",
-                    torchvision.utils.make_grid(images[0 : len(images) // 4]),
-                )
         progress.display(i)  # print final results
 
-    return top1.avg, top5.avg
+        # current_time = time.time()
+        # print(
+        #     'Test Loss {:.4f}\tmAP {:.2f}%\tf1 score {:.2f}\tTime {:.2f}s\n'
+        #     .format(float(losses.avg), float(mAP.avg*100), float(f1.avg), (current_time - start_time))
+        # )
+    return float(mAP.avg), float(f1.avg)
+
+# def base(model, device, val_loader, criterion, args, writer, epoch=0):
+#     """
+#         Evaluating on unmodified validation set inputs.
+#     """
+#     batch_time = AverageMeter("Time", ":6.3f")
+#     losses = AverageMeter("Loss", ":.4f")
+#     top1 = AverageMeter("Acc_1", ":6.2f")
+#     top5 = AverageMeter("Acc_5", ":6.2f")
+#     progress = ProgressMeter(
+#         len(val_loader), [batch_time, losses, top1, top5], prefix="Test: "
+#     )
+
+#     # switch to evaluate mode
+#     model.eval()
+
+#     with torch.no_grad():
+#         end = time.time()
+#         for i, data in enumerate(val_loader):
+#             images, target = data[0].to(device), data[1].to(device)
+
+#             # compute output
+#             output = model(images)
+#             loss = criterion(output, target)
+
+#             # measure accuracy and record loss
+#             acc1, acc5 = accuracy(output, target, topk=(1, 5))
+#             losses.update(loss.item(), images.size(0))
+#             top1.update(acc1[0], images.size(0))
+#             top5.update(acc5[0], images.size(0))
+
+#             # measure elapsed time
+#             batch_time.update(time.time() - end)
+#             end = time.time()
+
+#             if (i + 1) % args.print_freq == 0:
+#                 progress.display(i)
+
+#             if writer:
+#                 progress.write_to_tensorboard(
+#                     writer, "test", epoch * len(val_loader) + i
+#                 )
+
+#             # write a sample of test images to tensorboard (helpful for debugging)
+#             if i == 0 and writer:
+#                 writer.add_image(
+#                     "test-images",
+#                     torchvision.utils.make_grid(images[0 : len(images) // 4]),
+#                 )
+#         progress.display(i)  # print final results
+
+#     return top1.avg, top5.avg
 
 
 def adv(model, device, val_loader, criterion, args, writer, epoch=0):
